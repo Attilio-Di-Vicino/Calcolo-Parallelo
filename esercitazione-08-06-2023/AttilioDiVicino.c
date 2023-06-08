4b1e4be600a6a8adaeb5c1f01b30174a256d8a20
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-08 21:34:27 +0200

    es

--- esercitazione-08-06-2023/AttilioDiVicino.c
+++ esercitazione-08-06-2023/AttilioDiVicino.c
@@ -1,20 +1,20 @@
 /**
-* ESERCITAZIONE_AULA:
+* ESERCITAZIONE:
 * 
 * Implementare un programma parallelo per l'ambiente multicore con np
 * unità processanti che impieghi la libreria OpenMP. Il programma deve
 * essere organizzato come segue:
 *
-* * il core master deve leggere una matrice A di dimensione NxM
+* 1. il core master deve leggere una matrice A di dimensione NxM
 *
-* * i core devono organizzarsi per estrarre ognuno una sottomatrice,
-*   decomponendo la matrice A per blocchi riga, e costruendo la trasposta
-*   di tale sottomatrice, in una matrice Bloc
+* 2. i core devono organizzarsi per estrarre ognuno una sottomatrice,
+*    decomponendo la matrice A per blocchi riga, e costruendo la trasposta
+*    di tale sottomatrice, in una matrice Bloc
 *
-* * infine i core devono collaborare per sommare le sotto matrici estratte Bloc 
-*   in un'unica matrice finale C
+* 3. infine i core devono collaborare per sommare le sotto matrici estratte Bloc 
+*    in un'unica matrice finale C
 *
-* * il core master stampa la matrice risutato e il tempo d'esecuzione
+* 4. il core master stampa la matrice risutato e il tempo d'esecuzione
 */
 
 #include <omp.h>
@@ -25,7 +25,7 @@
 #define MAXVALUE 9
 #define N_BLOC M // le righe di Bloc saranno le colonne di A
 #define M_BLOC N // le colonne di Bloc saranno le righe di A
-#define NUMTHREADS 8 // Numero dei thread utilizzati
+#define NUMTHREADS 4 // Numero dei thread utilizzati
 #define OFFSET(b) b * ( N_BLOC / NUMTHREADS ) // Offset per accedere ai singoli Bloc
 #define N_C M / NUMTHREADS // Numero di righe della matrice C
 #define M_C N // Numero di colonne della matrice C
@@ -37,6 +37,18 @@ void printMatrix( int** matrix, int N, int M, char name[] ) {
             printf( "%d	", matrix[i][j] );
         printf( "\n" );
     }
+}
+
+void allocationMatrix( int*** matrix, int N, int M ) {
+    *matrix = ( int** ) calloc( N, sizeof( int* ) );
+	for ( int i = 0; i < N; i++ )
+		( *matrix )[i] = ( int* ) calloc( M, sizeof( int ) );
+}
+
+void initMatrix( int*** matrix, int N, int M ) {
+	for ( int i = 0; i < N; i++ )
+		for ( int j = 0; j < M; j++ )
+            ( *matrix )[i][j] = 0;
 }
 
 int main() {
@@ -71,37 +83,23 @@ int main() {
 	
 	printf( "\nMatrix -> N: %d, M: %d", N, M );
     printf( "\nBloc -> N: %d, M: %d", N_BLOC, M_BLOC );
-    printf( "\nNumero di thread utilizzati %d\n", NUMTHREADS );
+    printf( "\nNumero di thread utilizzati: %d\n", NUMTHREADS );
 	
-	// Allocazione matrice
-	matrix = ( int** ) calloc( N, sizeof( int* ) );
-	for ( int i = 0; i < N; i++ )
-		matrix[i] = ( int* ) calloc( M, sizeof( int ) );
+	// Allocazioni
+    allocationMatrix( &matrix, N, M );
+    allocationMatrix( &Bloc, N_BLOC, M_BLOC );
+    allocationMatrix( &C, N_C, M_C );
 	
-    // Allocazione matrici Bloc ( NB: sono più di una matrice )
-	Bloc = ( int** ) calloc( N_BLOC, sizeof( int* ) );
-	for ( int i = 0; i < N_BLOC; i++ )
-		Bloc[i] = ( int* ) calloc( M_BLOC, sizeof( int ) );
-
-    // Allocazione C
-    C = ( int** ) calloc( N_C, sizeof( int* ) );
-    for ( int i = 0; i < N_C; i++ )
-        C[i] = ( int* ) calloc( M_C, sizeof( int ) );
-	
-	// Inizializzazione matrice	
+	// Inizializzazioni	
 	for ( int i = 0; i < N; i++ )
 		for ( int j = 0; j < M; j++ )
 			matrix[i][j] = rand() % MAXVALUE + 1;
 
-    // Inizializzazione Bloc
-	for ( int i = 0; i < N_BLOC; i++ )
-		for ( int j = 0; j < M_BLOC; j++ )
-			Bloc[i][j] = 0;
+    initMatrix( &Bloc, N_BLOC, M_BLOC );
+    initMatrix( &C, N_C, M_C );
 
-    // Inizializzazione C
-	for ( int i = 0; i < N_C; i++ )
-		for ( int j = 0; j < M_C; j++ )
-			C[i][j] = 0;
+    // Prendiamo il tempo di inizio
+	t1 = omp_get_wtime();
 
 	// Calcolo della matrice trasposta
 	#pragma omp parallel for private(i,j) shared(Bloc,matrix) schedule(static) num_threads(NUMTHREADS)
@@ -110,23 +108,23 @@ int main() {
 			Bloc[j][i] = matrix[i][j];
 		}
 	}
-
-    // Prendiamo il tempo di inizio
-	t1 = omp_get_wtime();
 	
     // Stampa della matrice
 	#pragma omp master
-    printMatrix( matrix, N, M, "Matrix" );
-    // printMatrix( Bloc, N_BLOC, M_BLOC, "Trasposta" );
+    {
+        printMatrix( matrix, N, M, "Matrix" );
+    }
 
     // Stampa dei Bloc
     #pragma omp master
-    for ( b = 0; b < NUMTHREADS; b++ ) {
-        printf( "\nBloc #%d\n", b );
-        for ( i = 0; i < N_BLOC / NUMTHREADS; i++ ) {
-            for ( j = 0; j < M_BLOC; j++ )
-                printf( "%d    ", Bloc[ OFFSET(b) + i ][j] );
-            printf( "\n" );
+    {
+        for ( b = 0; b < NUMTHREADS; b++ ) {
+            printf( "\nBloc #%d\n", b );
+            for ( i = 0; i < N_BLOC / NUMTHREADS; i++ ) {
+                for ( j = 0; j < M_BLOC; j++ )
+                    printf( "%d    ", Bloc[ OFFSET(b) + i ][j] );
+                printf( "\n" );
+            }
         }
     }
 
@@ -139,15 +137,17 @@ int main() {
 		}
 	}
 
-    // Prendiamo il tempo di fine
-	t2 = omp_get_wtime();
-
     // Stampo C ed i tempi
     #pragma omp master
-    printMatrix( C, N_C, M_C, "Matrice finale C" );
-	printf( "\nStampa t1: %f", t1 );
-	printf( "\nStampa t2: %f", t2 );
-	printf( "\nTempo finale: %f", t2-t1 );
+    {
+        printMatrix( C, N_C, M_C, "Matrice finale C" );
+        printf( "\nStampa t1: %f", t1 );
+        printf( "\nStampa t2: %f", t2 );
+        printf( "\nTempo finale: %f", t2-t1 );
+    }
+
+    // Prendiamo il tempo di fine
+	t2 = omp_get_wtime();
 	
 	printf( "\n" );
 	return 0;
