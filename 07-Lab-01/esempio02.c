9f044769408a1f1519672d79981a16afa2acab01
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-22 10:49:36 +0200

    07-esercizi

---
+++ 07-Lab-01/esempio02.c
@@ -0,0 +1,14 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+
+    int id_thread, num_threads;
+    #pragma omp parallel private(id_thread), shared(num_threads)
+    {
+        id_thread = omp_get_thread_num();
+        num_threads = omp_get_num_threads();
+        printf( "Hello from thread %d, nthreads %d\n", id_thread, num_threads );
+    }
+    return 0;
+}
