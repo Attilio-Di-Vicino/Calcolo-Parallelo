83485f3f205212ceab051136c39aa9d2901411d8
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-22 17:28:55 +0200

    17 homework 1

--- 17-matrice-full-par-homework/01-homework.c
+++ 17-matrice-full-par-homework/01-homework.c
@@ -33,7 +33,7 @@ int main() {
     }
 
     printf( "\nMatrix:\n" );
-    for ( int i = 0; i < N; i++ ){
+    for ( int i = 0; i < N; i++ ) {
         for ( int j = 0; j < M; j++ )
             printf( "%d    ", matrix[i][j] );
         printf( "\n" );
