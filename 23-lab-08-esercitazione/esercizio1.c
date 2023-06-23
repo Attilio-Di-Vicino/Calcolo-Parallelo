1d89cce6f3df5fa7b10c3f2b77b8fe22ac14a355
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-23 12:30:06 +0200

    delete

---
+++ 23-lab-08-esercitazione/esercizio1.c
@@ -0,0 +1,5 @@
+/**
+ * Assegnata una matrice di dimensione N×N, estrarre in parallelo gli elementi
+ * della diagonale principale e ricopiarli in un vettore, quindi calcolarne il
+ * massimo, con una procedura parallela
+*/
