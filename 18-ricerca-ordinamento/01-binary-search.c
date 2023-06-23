9826897b0849a6cc8d579e6ef98ebdefb01990e3
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-23 10:37:48 +0200

    binary search and odd even sort

---
+++ 18-ricerca-ordinamento/01-binary-search.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define N 10
+
+int binarySearch( int, int, int, int, int* );
+int main() {
+    
+    int* vector;
+    vector = ( int* ) malloc( N * sizeof( int ) );
+
+    int key = 0;
+
+    do {
+        printf( "\nPlease, enter the key: " );
+        scanf( "%d", &key );
+    } while ( key > ( N - 1 ) || key < 0 );
+    
+    for ( int i = 0; i < N; i++ )
+        vector[i] = i;
+    
+    printf( "\nVector: " );
+    for ( int i = 0; i < N; i++ )
+        printf( "%d ", vector[i] );
+    
+    printf( "\nBinary Search result whit key %d is: %d", key, binarySearch( 0, N, key, N, vector ) );
+    
+    printf( "\n" );
+    return 0;
+}
+
+int binarySearch( int l, int r, int key, int n, int *a ) {
+    
+    int index = -1;
+    int size = ( r - l + 1 ) / n;
+    
+    if ( size == 0 || n == 1 ) {
+        #pragma omp parallel for
+        for ( int i = l; i <= r; i++ ) {
+            if ( a[i] == key )
+                index = i;
+        }
+        return index;
+    }
+
+    int left = l;
+    int right = r;
+    
+    omp_set_num_threads(n);
+    
+    /**
+     * Quando si chiama omp_set_nested(1) con il valore 1 come argomento,
+     * si imposta l'abilitazione delle regioni parallele annidate.
+     * Ciò significa che è possibile creare regioni parallele all'interno di
+     * altre regioni parallele e i thread possono essere creati per
+     * l'esecuzione di queste regioni annidate.
+    */
+    omp_set_nested(1);
+    
+    #pragma omp parallel
+    {
+        int id = omp_get_thread_num();
+        int lt = l + id * size;
+        int rt = lt + size - 1;
+        if ( id == n - 1 )
+            rt = r;
+        if ( a[lt] <= key && a[rt] >= key ) {
+            left = lt;
+            right = rt;
+        }
+    }
+
+    if ( left == l && right == r )
+        return -1;
+    
+    return binarySearch( left, right, key, n, a );
+}
---
+++ 18-ricerca-ordinamento/02-odd-even-sort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+#define SIZE 10
+#define MAXVALUE 100
+
+void printVector( int*, int, char[] );
+void oddEvenSort( int*, int);
+int main() {
+    
+    int* vector;
+    vector = ( int* ) malloc( SIZE * sizeof( int ) );
+    srand( time( NULL ) );
+    
+    for ( int i = 0; i < SIZE; i++ )
+        vector[i] = rand() % MAXVALUE + 1;
+    
+    printVector( vector, SIZE, "Vector before the sorting" );
+    oddEvenSort( vector, SIZE );
+    printVector( vector, SIZE, "Vector after the sorting" );
+    
+    printf( "\n" );
+    return 0;
+}
+
+void printVector( int* vector, int N, char name[] ) {
+    printf( "\n%s: ", name );
+    for ( int i = 0; i < SIZE; i++ )
+        printf( "%d ", vector[i] );
+}
+
+void oddEvenSort( int* a, int N ) {
+
+    int sw = 1, start = 0, i = 0;
+    int temp;
+    while( sw || start ) {
+        sw = 0 ;
+        
+        #pragma omp parallel for private( temp )
+        for ( i = start ; i < N - 1 ; i += 2) {
+            if ( a [i] > a [ i + 1 ] ) {
+                temp = a [ i ] ;
+                a [ i ] = a [ i +1];
+                a [ i +1] = temp ;
+                sw = 1 ;
+            }
+        }
+
+        if ( start == 0 ) 
+            start = 1 ;
+        else 
+            start = 0;
+    }
+}
