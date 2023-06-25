e08ed6b4f34f9a2f2053e13a14439a944bd9a796
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-25 13:21:49 +0200

    15 prodotto scalare 2 vettori

--- 15-lab-06-prodotto-scalare/esempio01.c
+++ 15-lab-06-prodotto-scalare/esempio01.c
@@ -3,6 +3,8 @@
 #include <omp.h>
 #include <time.h>
 
+#define MAXVALUE 100
+
 void printVector( float*, int, char[] );
 int main() {
     int i,N;
@@ -19,25 +21,39 @@ int main() {
     
     // Riempimento
     for ( i = 0; i < N; i++ )
-        a[i] = rand() % 100 + 1;
+        a[i] = rand() % MAXVALUE + 1;
     
     for ( i = 0; i < N; i++ )
-        b[i] = rand() % 100 + 1;
+        b[i] = rand() % MAXVALUE + 1;
 
     // Stampa 
     printVector( a, N, "Vector A:" );
     printVector( b, N, "Vector B:" );
     
     // direttiva nella versione più semplice possibile
+    /**
+     * In questo caso si sta lavorando su due vettori per eseguire un prodotto scalare
+     * di conseguenza inizialmente andremo a moltiplicare ogni elemento del primo vettore
+     * con il corrispettivo elemento del secondo vettore, quindi analogamente alla somma 
+     * di due vettori ( II Nucleo computazionale ), di conseguenza anche esso
+     * è un algoritmo full parallel e non c'è collezione dei dati;
+     * successivamente, andiamo ad eseguire la somma degli elementi
+     * risultanti dal prodotto, come nella somma di N numeri ( I Nucleo computazionale ),
+     * che sappiamo non essere un algorimto full parallel, di coseguenza dobbiamo adottare
+     * una strategia conveniente che ci permette di gestire questa situazione.
+     * In questo caso utilizziamo la seconda strategia attraverso la clausola reduction.
+    */
     #pragma omp parallel for private(i) shared (a,b) reduction(+:result)
     for ( i = 0; i < N; i++ )
-        result += ( a[i] * b[i] );
+        result += a[i] * b[i];
     
     printf( "\nRisultato = %f\n",result );
     
     // una buona abitudine è liberare sempre la memoria allocata alla fine del codice!
     free(a);
     free(b);
+
+    return 0;
 }
 
 void printVector( float* a, int N, char name[] ) {
