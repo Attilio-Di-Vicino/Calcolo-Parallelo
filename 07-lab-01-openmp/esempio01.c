e9533797cf61dcba75491532855d87a4025c56aa
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-25 13:00:50 +0200

    07 laboratorio 01 introduzione ad openMP

--- 07-lab-01-openmp/esempio01.c
+++ 07-lab-01-openmp/esempio01.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 int main() {
+
     #pragma omp parallel
     printf( "Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads() );
+    
     return 0;
 }
--- 07-lab-01-openmp/esempio02.c
+++ 07-lab-01-openmp/esempio02.c
@@ -4,11 +4,13 @@
 int main() {
 
     int id_thread, num_threads;
+
     #pragma omp parallel private(id_thread), shared(num_threads)
     {
         id_thread = omp_get_thread_num();
         num_threads = omp_get_num_threads();
         printf( "Hello from thread %d, nthreads %d\n", id_thread, num_threads );
     }
+    
     return 0;
 }
